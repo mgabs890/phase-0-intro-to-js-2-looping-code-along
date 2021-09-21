c3b1006c631521819ad95b97b6f689de4f6df2c5
Gabriella Delgado <m.gabriella.delgado@gmail.com>
2021-09-20 22:49:21 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards(namesArray, event){
+    let thankYouCards = []
+    for (let i=0; i<namesArray.length; i++){
+        thankYouCards.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYouCards
+}
+
+writeCards(['nick'], 'birthday');
+
+function countDown(startingNumber){
+    let countDown = 10;
+    while (startingNumber>0){
+        console.log(startingNumber);
+        startingNumber -- ;
+    }
+    console.log(startingNumber);
+}
